16d9198d6374c248c5dea130bd55b1208af4e483
zachisoni <muhammad.asaduddin.1.19.1.4@gmail.com>
2022-12-09 22:15:55 +0700

    Path Patch

--- enumerate_example.py
+++
@@ -1,6 +0,0 @@
-if __name__ == '__main__':
-    list_data = ["A", "B", "C", "D", "E", "F"]
-
-    
-    for baris, l in enumerate(list_data):
-        print(baris, "=", l)
--- utils/preprocessor.py
+++ utils/preprocessor.py
@@ -1,42 +1,44 @@
+import enum
 import pickle
-import re
 import torch
 import os
+
+import re
+
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
 
 import pytorch_lightning as pl
 import pandas as pd
 
-from select import select
-from shutil import which
-from numpy import mat
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
-
-from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
-from tqdm import tqdm
 from transformers import BertTokenizer
+
+from tqdm import tqdm
 
 class PreprocessorClass(pl.LightningDataModule):
 
     def __init__(self, 
-                preprocessed_dir,
-                batch_size = 10,
-                max_length = 100):
-        #variable label2id untuk mapping
+                 preprocessed_dir,
+                 batch_size = 10,
+                 max_length = 100):
+
+        super(PreprocessorClass, self).__init__()
         self.label2id = {
-            'bola' : 0,
-            'news' : 1,
-            'bisnis' : 2,
-            'tekno' : 3,
-            'otomotif' : 4
+            'bola': 0,
+            'news': 1,
+            'bisnis': 2,
+            'tekno': 3,
+            'otomotif': 4
         }
+
         factory = StemmerFactory()
-        self.stemmer = factory.create_stemmer() 
+        self.stemmer = factory.create_stemmer()
 
         self.tokenizer = BertTokenizer.from_pretrained('indolem/indobert-base-uncased')
         self.max_length = max_length
         self.preprocessed_dir = preprocessed_dir
-        self.batch_size = batch_size
 
+        self.batch_size = batch_size
 
     def clean_str(self, string):
         string = string.lower()
@@ -58,35 +60,30 @@ class PreprocessorClass(pl.LightningDataModule):
         # Menghilangkan imbuhan
         return self.stemmer.stem(string)
 
-    #memuat data sesuai dengan file yg ada dan memasukkan menjadi variable setelah as 
     def load_data(self,):
-       #keyword with seperti try..exception tetapi pada proses file
-        with open("https://github.com/zachisoni/bert_classiffication_sem3/tree/main/data/training.res", "rb") as tdr:
+        with open("bert_classification_semester3/data/training.res", "rb") as tdr:
             train_pkl = pickle.load(tdr)
-            train = pd.DataFrame({'title':train_pkl[0], 'label' : train_pkl[1]})
-        with open("https://github.com/zachisoni/bert_classiffication_sem3/tree/main/data/testing.res", "rb") as tsdr:
+            train = pd.DataFrame({'title': train_pkl[0], 'label': train_pkl[1]})
+        with open("bert_classification_semester3/data/testing.res", "rb") as tsdr:
             test_pkl = pickle.load(tsdr)
-            test = pd.DataFrame({'title': test_pkl[0], 'label' : test_pkl[1]})
-
+            test = pd.DataFrame({'title': test_pkl[0], 'label': test_pkl[1]})
         
-        #mapping masing-masing label dengan angka 
+        # Mengetahui apa saja label yang ada di dalam dataset
+        label_yang_ada = train["label"].drop_duplicates()
+
+        # Konversi dari label text (news) ke label id (1)
         train.label = train.label.map(self.label2id)
         test.label = test.label.map(self.label2id)
 
-
-        # print(train)
-
         return train, test
-
-    #mengurutkan data
+    
     def arrange_data(self, data, type):
-        #Algoritma :
-        #1. Cleaning Sentence : menghilangkan karakter-karakter yang tidak seharusnya ada seperti emoji
-        #2. Tokenizing : mengubah kata-kata menjadi id
-        #3. Arrage ke dataset  (training, validation, testing)
+        # Yang di lakukan
+        # 1. Cleaning sentence
+        # 2. Tokenizing
+        # 3. Arrange ke dataset (training, validation, testing)
 
-        x_input_ids, x_token_type_id, x_attention_mask, y = [], [], [], []
-        
+        x_input_ids, x_token_type_ids, x_attention_mask, y = [], [], [], []
         for baris, dt in enumerate(tqdm(data.values.tolist())):
             title = self.clean_str(dt[0])
             label = dt[1]
@@ -95,70 +92,72 @@ class PreprocessorClass(pl.LightningDataModule):
             binary_lbl[label] = 1
 
             tkn = self.tokenizer(text = title,
-                                max_length = self.max_length,
-                                padding = "max_length",
-                                truncation = True)
+                                 max_length = self.max_length, 
+                                 padding = "max_length",
+                                 truncation = True)
 
-            # print(tkn["input_ids"])
             x_input_ids.append(tkn['input_ids'])
-            x_token_type_id.append(tkn['token_type_ids'])
+            x_token_type_ids.append(tkn['token_type_ids'])
             x_attention_mask.append(tkn['attention_mask'])
             y.append(binary_lbl)
-
-            if baris > 10 :
+            
+            if baris > 10:
                 break
-        
+
         x_input_ids = torch.tensor(x_input_ids)
-        x_token_type_id  = torch.tensor(x_token_type_id)
+        x_token_type_ids = torch.tensor(x_token_type_ids)
         x_attention_mask = torch.tensor(x_attention_mask)
         y = torch.tensor(y)
 
         tensor_dataset = TensorDataset(x_input_ids,
-                                       x_token_type_id,
+                                       x_token_type_ids,
                                        x_attention_mask,
                                        y)
-        
+
         if type == "train":
-            train_tensor_dataset, valid_tensor_dataset = torch.utils.data.random_split(tensor_dataset,[
-                                                            round(len(x_input_ids) * 0.8),
+            # Standard split : Train (80%), Validation (20%)
+            train_tensor_dataset, valid_tensor_dataset = torch.utils.data.random_split(tensor_dataset, [
+                                                            round(len(x_input_ids) * 0.8), 
                                                             len(x_input_ids) - round(len(x_input_ids) * 0.8)
-                                                        ])
+                                                         ])
+            
             torch.save(train_tensor_dataset, f"{self.preprocessed_dir}/train.pt")
             torch.save(valid_tensor_dataset, f"{self.preprocessed_dir}/valid.pt")
-            
+
             return train_tensor_dataset, valid_tensor_dataset
-        else :
-            torch.save(tensor_dataset, f"{self.preprocessed_dir}/valid.pt")
+        else:
+            torch.save(tensor_dataset, f"{self.preprocessed_dir}/test.pt")
             return tensor_dataset
 
-    def preprocessor(self,):
+    def preprocessor(self):
         train, test = self.load_data()
 
         if not os.path.exists(f"{self.preprocessed_dir}/train.pt") or not os.path.exists(f"{self.preprocessed_dir}/valid.pt"):
             print("Create Train and Validation dataset")
             train_data, valid_data = self.arrange_data(data = train, type = "train")
-        else :
+        else:
             print("Load Preprocessed train and validation data")
             train_data = torch.load(f"{self.preprocessed_dir}/train.pt")
             valid_data = torch.load(f"{self.preprocessed_dir}/valid.pt")
-        
+
         if not os.path.exists(f"{self.preprocessed_dir}/test.pt"):
             print("Create test dataset")
             test_data = self.arrange_data(data = test, type = "test")
-        else :
+        else:
             print("Load Preprocessed test data")
-            test_data = torch.load(f"{self.preprocessed_dir}/valid.pt")
+            test_data = torch.load(f"{self.preprocessed_dir}/test.pt")
 
         return train_data, valid_data, test_data
 
     def setup(self, stage = None):
         train_data, valid_data, test_data = self.preprocessor()
-        if stage == "fit" :
+        print(valid_data)
+        if stage == "fit":
             self.train_data = train_data
             self.valid_data = valid_data
         elif stage == "predict":
             self.test_data = test_data
-    
+
     def train_dataloader(self):
         sampler = RandomSampler(self.train_data)
         return DataLoader(
@@ -167,9 +166,9 @@ class PreprocessorClass(pl.LightningDataModule):
             sampler = sampler,
             num_workers = 1
         )
-
+    
     def val_dataloader(self):
-        sampler = RandomSampler(self.valid_data)
+        sampler = SequentialSampler(self.valid_data)
         return DataLoader(
             dataset = self.valid_data,
             batch_size = self.batch_size,
@@ -178,18 +177,16 @@ class PreprocessorClass(pl.LightningDataModule):
         )
 
     def predict_dataloader(self):
-        sampler = RandomSampler(self.train_data)
+        sampler = SequentialSampler(self.test_data)
         return DataLoader(
             dataset = self.test_data,
             batch_size = self.batch_size,
             sampler = sampler,
             num_workers = 1
         )
-
-
-if __name__ == '__main__':
-    Pre = PreprocessorClass(preprocessed_dir = "data/preprocessed")
-    Pre.setup(stage = "fit")
-    train_data = Pre.train_dataloader()
-    print(train_data)
-
+    
+# if __name__ == '__main__':
+#     Pre = PreprocessorClass(preprocessed_dir = "data/preprocessed")
+#     Pre.setup(stage = "fit")
+#     train_data = Pre.train_dataloader()
+    
