3dffa19e7e39923f490ab851213048c50969a995
zachisoni <muhammad.asaduddin.1.19.1.4@gmail.com>
2022-12-17 10:47:57 +0700

    add parser function

--- multi_class.py
+++ multi_class.py
@@ -1,8 +1,23 @@
+import argparse
+
 from utils.preprocessor import PreprocessorClass
 from models.multi_class_model import MultiClassModel
 
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import TensorBoardLogger
+
+def collect_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--accelerator", type=str, default='gpu')
+    parser.add_argument("--gpu_id", type=int, default=0)
+    parser.add_argument("--num_nodes", type=int, default=1)
+    parser.add_argument("--batch_size", type=int, default=10)
+    parser.add_argument("--max_length", type=int, default=100)
+    parser.add_argument("--test_data_dir", type=str, default="data/testing.res")
+    parser.add_argument("--test_data_dir", type=str, default="data/training.res")
+
+    return parser.parse_args()
 
 if __name__ == '__main__':
     dm = PreprocessorClass(preprocessed_dir = "bert_classification_sem3/sata/preprocessed",
