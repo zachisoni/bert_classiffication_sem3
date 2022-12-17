2ea6bda3ae0559a5146869536c1b9ea6cbd6442c
zachisoni <muhammad.asaduddin.1.19.1.4@gmail.com>
2022-12-17 10:33:10 +0700

    add parser

--- multi_class.py
+++ multi_class.py
@@ -1,13 +1,32 @@
+import argparse
+
 from utils.preprocessor import PreprocessorClass
 from models.multi_class_model import MultiClassModel
 
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import TensorBoardLogger
 
+def collect_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--accelerator", type=str, default='gpu')
+    parser.add_argument("--gpu_id", type=int, default=0)
+    parser.add_argument("--num_nodes", type=int, default=1)
+    parser.add_argument("--batch_size", type=int, default=10)
+    parser.add_argument("--max_length", type=int, default=100)
+    parser.add_argument("--test_data_dir", type=str, default="data/testing.res")
+    parser.add_argument("--test_data_dir", type=str, default="data/training.res")
+
+    return parser.parse_args()
+
 if __name__ == '__main__':
+    args = collect_parser()
+
     dm = PreprocessorClass(preprocessed_dir = "data/preprocessed",
-                           batch_size = 100,
-                           max_length = 100)
+                           train_data_dir= args.train_data_dir,
+                           test_data_dir= args.test_data_size,
+                           batch_size = args.batch_size,
+                           max_length = args.max_length)
 
     # Learning rate diganti 1e-3 ke 1e-5
     model = MultiClassModel(
@@ -19,7 +38,9 @@ if __name__ == '__main__':
     logger = TensorBoardLogger("logs", name="bert-multi-class")
 
     trainer = pl.Trainer(
-        gpus = 1,
+        accelerator= args.accelerator,
+        gpus = args.gpu_id,
+        num_nodes=args.num_nodes,
         max_epochs = 10,
         default_root_dir = "checkpoints/class"
     )
