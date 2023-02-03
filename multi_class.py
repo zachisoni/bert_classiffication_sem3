02efc6b870639b2693fa9af3cdad244f16b8b58b
zachisoni <muhammad.asaduddin.1.19.1.4@gmail.com>
2023-02-03 11:10:33 +0700

    change typos

--- multi_class.py
+++ multi_class.py
@@ -5,6 +5,7 @@ from models.multi_class_model import MultiClassModel
 
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import TensorBoardLogger
+import torchmetrics
 
 def collect_parser():
     parser = argparse.ArgumentParser()
@@ -14,8 +15,8 @@ def collect_parser():
     parser.add_argument("--num_nodes", type=int, default=1)
     parser.add_argument("--batch_size", type=int, default=10)
     parser.add_argument("--max_length", type=int, default=100)
-    parser.add_argument("--test_data_dir", type=str, default="data/testing.res")
-    parser.add_argument("--test_data_dir", type=str, default="data/training.res")
+    parser.add_argument("--test_data_dir", type=str, default="bert_classification_sem3data/testing.res")
+    parser.add_argument("--train_data_dir", type=str, default="bert_classification_sem3data/training.res")
 
     return parser.parse_args()
 
@@ -24,7 +25,7 @@ if __name__ == '__main__':
 
     dm = PreprocessorClass(preprocessed_dir = "bert_classification_sem3/data/preprocessed",
                            train_data_dir= args.train_data_dir,
-                           test_data_dir= args.test_data_size,
+                           test_data_dir= args.test_data_dir,
                            batch_size = args.batch_size,
                            max_length = args.max_length)
 
@@ -48,4 +49,6 @@ if __name__ == '__main__':
 
     trainer.fit(model, datamodule = dm)
     # pred, true = trainer.predict(model = model, datamodule = dm)
+    hasil = trainer.predict(model= model, datamodule= dm)
+
 
